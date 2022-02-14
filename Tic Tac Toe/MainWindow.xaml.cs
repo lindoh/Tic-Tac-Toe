2a9ddbabeb7e024e82beb52be0432da2d80fcfc1
Lindo Gamede <lindohgamede@gmail.com>
2022-02-14 22:42:59 +0200

    Added Private members and a newGame method

--- Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace Tic_Tac_Toe
 {
@@ -20,9 +8,42 @@ namespace Tic_Tac_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+        /// <summary>
+        /// Will store the current mark type for each cell
+        /// </summary>
+        MarkType[] Results;
+
+        /// <summary>
+        /// True when the Player is playing and False when computer plays
+        /// </summary>
+        private bool PlayerTurn;
+
+        /// <summary>
+        /// True when the Game is over
+        /// </summary>
+        private bool GameOver;
+
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+        #endregion
+
+        private void NewGame()
+        {
+            //Create an array of 9 cells
+            Results = new MarkType[9];
+
+            //Make all cells markers Free
+            for (int i = 0; i < Results.Length; i++)
+                Results[i] = MarkType.Free;
         }
     }
 }
---
+++ Tic Tac Toe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Tic_Tac_Toe
+{
+    public enum MarkType
+    {
+        /// <summary>
+        /// An empty cell
+        /// </summary>
+        Free,
+
+        /// <summary>
+        /// A player mark (An 'X' is displayed when a player plays)
+        /// </summary>
+        Cross,
+
+        /// <summary>
+        /// A computer mark ('O' is displayed after computer plays)
+        /// </summary>
+        Nought
+    }
+}
